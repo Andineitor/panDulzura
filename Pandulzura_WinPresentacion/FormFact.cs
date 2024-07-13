00c36f3adf14b713a11eb32af49ccb9342cfeae9
Jael <jhael.nicolalde@epn.edu.ec.com>
2024-07-12 22:42:57 -0500

    se añade factura

---
+++ Pandulzura_WinPresentacion/FormFact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pandulzura_WinPresentacion
+{
+    public partial class FormFact : Form
+    {
+        public FormFact()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
