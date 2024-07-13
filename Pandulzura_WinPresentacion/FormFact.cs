c8ea1884993fb85446ac10c9bac2fc53e487857c
Jael <jhael.nicolalde@epn.edu.ec.com>
2024-07-12 22:50:09 -0500

    se añade factura

--- Pandulzura_WinPresentacion/FormFact.cs
+++ Pandulzura_WinPresentacion/FormFact.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pandulzura_AccesoDatos.Entidades;
+using Pandulzura_LogicaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,33 @@ namespace Pandulzura_WinPresentacion
 {
     public partial class FormFact : Form
     {
+
+        private FacturaLogica factLogica;
+        private Factura nuevaFac;
+
         public FormFact()
         {
             InitializeComponent();
+            factLogica = new FacturaLogica();
+            nuevaFac = new Factura();
         }
+
+        //Listar
+        public void listarRol()
+        {
+            
+        }
+
+        //Insertar
+        public void insertar()
+        {
+            
+        }
+        private void LimpiarCampos()
+        {
+
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
